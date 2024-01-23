aec89b42c7babb7fa21f5f5e3f9f953770b02c70
@Logu@2003 <tlogeshwaran2003@gmail.com>
2024-01-23 23:11:15 +0530

    CorsUpdate

--- Model/SigninCheck.js
+++ Model/SigninCheck.js
@@ -1,9 +1,13 @@
 const jwt = require("jsonwebtoken");
 const LoginSchema = require("../LoginSchema");
+const error_handler = require("../errorHandling");
 
-const SignupCheck = async (req, res) => {
+const SignupCheck = async (req, res, next) => {
   const cookie = process.env.COOKIE_NAME;
   const cookies = req.cookies;
+  if (!cookies) {
+    return next(error_handler(404, "Coookie not found"));
+  }
   const token = cookies[cookie];
 
   if (!token) {

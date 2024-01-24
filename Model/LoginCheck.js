45bbddc97b8f2e15433938998065960e33d19556
@Logu@2003 <tlogeshwaran2003@gmail.com>
2024-01-24 10:21:02 +0530

    CorsUpdate

--- Model/LoginCheck.js
+++ Model/LoginCheck.js
@@ -18,7 +18,7 @@ const Logincheck = async (req, res, next) => {
     httpOnly: true,
     sameSite: "none",
     secure: true,
-    domain: "expressyourthought.vercel.app",
+    domain: "expressyourthought.vercel.app.",
   });
 
   res.json({ data: jwt, msg: "Login succesfull", cookie: COOKIE_NAME });

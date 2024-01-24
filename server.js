e456b853d5e19e7fee07efee21282f3d33acb0c0
@Logu@2003 <tlogeshwaran2003@gmail.com>
2024-01-24 10:43:45 +0530

    CorsUpdate

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(
     origin: ["https://expressyourthought.vercel.app", "http://localhost:3000"],
     methods: ["GET", "POST", "DELETE"],
     credentials: true,
+    allowedHeaders: ["Set-Cookie"],
   })
 );
 app.use(express.json({ limit: "10mb" }));

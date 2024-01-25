e7d0515315e2bd3f7481bbd20ea980a6b8aab8e0
@Logu@2003 <tlogeshwaran2003@gmail.com>
2024-01-25 23:54:42 +0530

    1domainset

--- Model/LoginCheck.js
+++ Model/LoginCheck.js
@@ -13,14 +13,16 @@ const Logincheck = async (req, res, next) => {
     expiresIn: "3days",
   });
 
-  await res.cookie(COOKIE_NAME, jwt, {
-    path: "/",
-    expires: new Date(Date.now() + 1000 * 60 * 58 * 52),
-    httpOnly: true,
-    sameSite: "none",
-    domain: "www.ideavista.online",
-    secure: true,
-  });
+  await res
+    .cookie(COOKIE_NAME, jwt, {
+      path: "/",
+      expires: new Date(Date.now() + 1000 * 60 * 58 * 52),
+      httpOnly: true,
+      sameSite: "none",
+      domain: "www.ideavista.online",
+      secure: true,
+    })
+    .send();
 
   res.json({ data: jwt, msg: "Login succesfull", cookie: COOKIE_NAME });
 };

cb39f70ada13d8a5d842dfc2093ecf3c67a7986e
@Logu@2003 <tlogeshwaran2003@gmail.com>
2024-01-25 22:08:16 +0530

    domainset

--- Model/LoginCheck.js
+++ Model/LoginCheck.js
@@ -17,7 +17,7 @@ const Logincheck = async (req, res, next) => {
     expires: new Date(Date.now() + 1000 * 60 * 58 * 52),
     httpOnly: true,
     sameSite: "none",
-    domain: ["www.ideavista.online", "localhost"],
+    domain: "www.ideavista.online",
     secure: true,
   });
 

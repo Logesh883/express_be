47026c8ad61801c2b4bef3ece514d5e7ee0aaa04
@Logu@2003 <tlogeshwaran2003@gmail.com>
2024-01-26 00:53:52 +0530

    2domainset

--- Logout.js
+++ Logout.js
@@ -1,7 +1,10 @@
 const Logout = async (req, res, next) => {
   const id = req.id;
   if (id) {
-    res.clearCookie(process.env.COOKIE_NAME);
+    res.clearCookie(process.env.COOKIE_NAME, {
+      domain: ".ideavista.online",
+      path: "/",
+    });
     req.cookies[process.env.COOKIE_NAME] = "";
     res.json({ msg: "Successfully Logout" });
   }

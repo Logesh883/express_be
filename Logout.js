5509098e94a7081af941630c68120d4f5b0261e8
@Logu@2003 <tlogeshwaran2003@gmail.com>
2024-01-26 16:22:53 +0530

    3domainset

--- Logout.js
+++ Logout.js
@@ -1,6 +1,7 @@
 const Logout = async (req, res, next) => {
   const id = req.id;
   if (id) {
+    res.setHeader("Cache-Control", "no-store");
     req.cookies[process.env.COOKIE_NAME] = "";
     return res
       .clearCookie(process.env.COOKIE_NAME, {
